6482dde956391a8db47457d4988458414ac8dcac
teguhalficahlin-del <teguhalficahlin@gmail.com>
2026-05-10 11:43:55 +0700

    Update CACHE_VERSION to flaf-v36

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // WAJIB update CACHE_VERSION setiap deploy
 // ============================================================
 
-const CACHE_VERSION  = 'flaf-v35';          // ← dinaikkan dari v4
+const CACHE_VERSION  = 'flaf-v36';          // ← dinaikkan dari v4
 const PDF_CACHE_NAME = 'flaf-pdf-v1';
 
 const APP_SHELL = [
