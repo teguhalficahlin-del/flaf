428764338370d6a3273e3cd6f7066197afa5b2bd
teguhalficahlin-del <teguhalficahlin@gmail.com>
2026-05-10 17:14:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // WAJIB update CACHE_VERSION setiap deploy
 // ============================================================
 
-const CACHE_VERSION  = 'flaf-v40';          // ← dinaikkan untuk apply path fix
+const CACHE_VERSION  = 'flaf-v41';          // ← dinaikkan untuk apply path fix
 const PDF_CACHE_NAME = 'flaf-pdf-v1';
 
 const APP_SHELL = [
