41f666b0ceb1a834d5494fafa7a44c200395108c
teguhalficahlin-del <teguhalficahlin@gmail.com>
2026-05-11 13:06:31 +0700

    Increment CACHE_VERSION to flaf-v47
    Updated CACHE_VERSION to apply path fix.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // WAJIB update CACHE_VERSION setiap deploy
 // ============================================================
 
-const CACHE_VERSION  = 'flaf-v46';          // ← dinaikkan untuk apply path fix
+const CACHE_VERSION  = 'flaf-v47';          // ← dinaikkan untuk apply path fix
 const PDF_CACHE_NAME = 'flaf-pdf-v1';
 
 const APP_SHELL = [
