c93b369557bdd487be56be3ec0902cb39500e1df
teguhalficahlin-del <teguhalficahlin@gmail.com>
2026-05-12 09:52:50 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // WAJIB update CACHE_VERSION setiap deploy
 // ============================================================
 
-const CACHE_VERSION  = 'flaf-v51';          // ← paksa refresh cache nilai.js
+const CACHE_VERSION  = 'flaf-v52';          // ← paksa refresh cache nilai.js
 const PDF_CACHE_NAME = 'flaf-pdf-v1';
 
 const APP_SHELL = [
